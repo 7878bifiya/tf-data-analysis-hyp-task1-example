a66dffa5a170f072026e0a98be243bfdc962c69b
7878bifiya <45232417+7878bifiya@users.noreply.github.com>
2023-04-11 20:28:56 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,19 @@
-import pandas as pd
-import numpy as np
-
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
-
-def solution(x_success: int, 
-             x_cnt: int, 
-             y_success: int, 
-             y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(x_success: int, x_cnt: int, y_success: int, y_cnt: int) -> bool:
+    alpha=0.03
+    control_conversion = x_success / x_cnt
+    test_conversion = y_success / y_cnt
+    
+    # Вычисляем комбинированную пропорцию
+    combined_conversion = (x_success + y_success) / (x_cnt + y_cnt)
+    
+    # Вычисляем стандартную ошибку
+    se = np.sqrt(combined_conversion * (1 - combined_conversion) * (1/x_cnt + 1/y_cnt))
+    
+    # Вычисляем Z-статистику
+    z = (control_conversion - test_conversion) / se
+    
+    # Находим критическое значение Z-статистики
+    critical_value = norm.ppf(1 - alpha/2) # двусторонний Z-тест
+    
+    # Принимаем решение на основе Z-статистики
+    return abs(z) > critical_value
