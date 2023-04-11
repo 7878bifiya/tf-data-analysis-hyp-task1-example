3c24c576bae484d0d2070785fd132ddf17bec107
7878bifiya <45232417+7878bifiya@users.noreply.github.com>
2023-04-11 21:01:23 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,3 +1,11 @@
+
+import pandas as pd
+import numpy as np
+from scipy import stats
+from scipy.optimize import minimize_scalar
+
+chat_id = 541133397 # Ваш chat ID, не меняйте название переменной
+
 def solution(x_success: int, x_cnt: int, y_success: int, y_cnt: int) -> bool:
     alpha=0.03
     control_conversion = x_success / x_cnt
